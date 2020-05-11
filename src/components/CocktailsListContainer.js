4ae509558d37b6117844c3936e6e168a61c0213b
gergokutu <gergo.kutu@gmail.com>
2020-05-11 11:01:28 +0200

    Move hardcoded categories to local state

--- src/components/CocktailsListContainer.js
+++ src/components/CocktailsListContainer.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import CocktailsList from './CocktailsList';
 
 export default class CocktailsListContainer extends Component {
+  state = {
+    categories: [ "category 1", "category 2", "category 3" ]
+  };
+
   render() {
     return (
-      <CocktailsList cocktailCategories={[ "category 1", "category 2", "category 3" ]} />
+      <CocktailsList cocktailCategories={this.state.categories} />
     );
   }
 }
