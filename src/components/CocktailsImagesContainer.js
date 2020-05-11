46ea682011f9de3ff9a90e02dc70dd1faa3d1954
gergokutu <gergo.kutu@gmail.com>
2020-05-11 14:11:06 +0200

    Fetch cocktail category data

--- src/components/CocktailsImagesContainer.js
+++ src/components/CocktailsImagesContainer.js
@@ -6,6 +6,17 @@ export default class CocktailsImagesContainer extends Component {
     cocktails: [] 
   };
 
+  componentDidMount() {
+    const { categoryName } = this.props.match.params;
+
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${encodeURIComponent(categoryName)}`)
+      .then(response => response.json())
+      .then(data => this.updateCocktails(data.drinks))
+      .catch(console.error);
+  };
+
+  updateCocktails = cocktailsToUpdate => this.setState({ cocktails: cocktailsToUpdate });
+
   render() {
     return (
       <CocktailsImages cocktails={this.state.cocktails} />
