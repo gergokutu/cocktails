1a04295271b331b6d1cac44bbec615640c9c1b74
gergokutu <gergo.kutu@gmail.com>
2020-05-12 10:57:19 +0200

    Show cocktail details on the page

--- src/components/CocktailDetails.js
+++ src/components/CocktailDetails.js
@@ -2,10 +2,36 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function CocktailDetails(props) {
+  const { 
+    idDrink,
+    strDrink,
+    strDrinkThumb,
+    strInstructions } = props.cocktail !== null && props.cocktail[0];
+  
+  const { ingredients } = props;
+  
   return (
-    <div>
-      <h2>DETAILS COMES HERE...</h2>
-      <Link to="/">Go back to index</Link>
+    <div className="cocktail-details">
+      <h2>{strDrink}</h2>
+      
+      <div className="cocktail-image">
+        <img src={strDrinkThumb} alt={strDrink}/>
+      </div>
+
+      <p className="instructions">{strInstructions}</p>
+
+      <div className="ingredients">
+        <h3>Ingredients:</h3>
+        <ul>
+          { ingredients.map((ingredient, index) => (
+            <li key={index}>{ingredient}</li>))
+          }
+        </ul>
+      </div>
+
+      <div className="back-link">
+          <Link to="/">Go back to the index.</Link>
+      </div>
     </div>
   );
 }
