2e7c5e78450a9cc9404421c79afd65c079a42d9c
gergokutu <gergo.kutu@gmail.com>
2020-05-12 11:42:50 +0200

    Show ingredient images

--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -6,7 +6,10 @@ export default function Ingredient(props) {
   return (
     <li className="ingredient" key={index}>
       <div className="ingredient-image">
-        <img src="#" alt={ingredient} />
+        <img
+          src={`https://www.thecocktaildb.com/images/ingredients/${encodeURIComponent(ingredient)}-Small.png`}
+          alt={ingredient}
+        />
       </div>
       <p>{ingredient}</p>
     </li>
