90da81b67b4155cd3f4994e0ae6e8045776411c5
gergokutu <gergo.kutu@gmail.com>
2020-05-12 09:43:51 +0200

    Initialize CocktailDetailsContainer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import CocktailsListContainer from './components/CocktailsListContainer';
 import { Route } from 'react-router-dom';
 import CocktailsImagesContainer from './components/CocktailsImagesContainer';
-import CocktailDetails from './components/CocktailDetails';
+import CocktailDetailsContainer from './components/CocktailDetailsContainer';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <main>
         <Route exact path="/" component={CocktailsListContainer} />
         <Route path="/categories/:categoryName" component={CocktailsImagesContainer} />
-        <Route path="/cocktails/:cocktailID" component={CocktailDetails} />
+        <Route path="/cocktails/:cocktailID" component={CocktailDetailsContainer} />
       </main>
     </div>
   );
---
+++ src/components/CocktailDetailsContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import CocktailDetails from './CocktailDetails';
+
+export default class CocktailDetailsContainer extends Component {
+  render() {
+    return (
+      <CocktailDetails />
+    );
+  }
+}
