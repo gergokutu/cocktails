86222cc51d1a607d8e1873b901d8f77d149b9d74
gergokutu <gergo.kutu@gmail.com>
2020-05-12 14:56:09 +0200

    Implement common fullscreen background through html element

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 import CocktailsListContainer from './components/CocktailsListContainer';
 import { Route } from 'react-router-dom';
 import CocktailsImagesContainer from './components/CocktailsImagesContainer';
